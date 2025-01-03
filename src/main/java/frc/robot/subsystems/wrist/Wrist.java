fa4abed593e0da5d98aeabff73ede7fa0046429b
jamesb1328283 <jamespbae@gmail.com>
2025-01-02 23:51:09 -0500

    challenge 4 hip hip hooray

--- src/main/java/frc/robot/subsystems/wrist/Wrist.java
+++ src/main/java/frc/robot/subsystems/wrist/Wrist.java
@@ -3,36 +3,47 @@ package frc.robot.subsystems.wrist;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Wrist extends SubsystemBase {
   // For instructions on how to implement this class, refer to the README.md file
+  // no idiot
+
+  public WristIO m_io;
+  public WristInputsAutoLogged m_inputs;
+  private PIDController m_PIDController;
 
   public Wrist(WristIO io, PIDController controller) {
-    // TODO: Implement the constructor
+    m_io = io;
+    m_PIDController = controller;
+    m_inputs = new WristInputsAutoLogged();
   }
 
   @Override
   public void periodic() {
-    // TODO: Implement this method
+    m_io.updateInputs(m_inputs);
+    double PID = m_PIDController.calculate(m_io.getAngle().getRadians());
+    m_io.setVoltage(PID);
+    ;
   }
 
   public void setDesiredAngle(Rotation2d angle) {
-    // TODO: Implement this method
+    m_PIDController.setSetpoint(angle.getRadians());
   }
 
   public Command setDesiredAngleCommand(Rotation2d angle) {
-    // TODO: Implement this method
-    return null;
+    return Commands.runOnce(
+        () -> {
+          setDesiredAngle(angle);
+        });
   }
 
   public boolean withinTolerance() {
-    // TODO: Implement this method
-    return false;
+    return m_PIDController.atSetpoint();
   }
 
   public WristInputsAutoLogged getInputs() {
-    // TODO: Implement this method
-    return null;
+    return m_inputs;
   }
 }
